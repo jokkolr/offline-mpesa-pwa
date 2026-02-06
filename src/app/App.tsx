8b9805c4521795942829a4259a2e95b9b273548b
jokkolr <169060103+jokkolr@users.noreply.github.com>
2026-02-06 13:34:58 +0300

    Add base App component

---
+++ src/app/App.tsx
@@ -0,0 +1,11 @@
+export default function App() {
+  return (
+    <main>
+      <h1>Offline M-Pesa PWA</h1>
+      <p>
+        This application is designed to work reliably in low or unstable
+        network conditions.
+      </p>
+    </main>
+  );
+}
